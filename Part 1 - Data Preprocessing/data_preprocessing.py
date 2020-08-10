086a193993b4377976e22adfdc66109c9795ad71
jamescardona11 <jjcardonao@gmail.com>
2020-08-10 16:26:45 -0500

    Load dataset

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 10 14:41:38 2020
+Created on Mon Aug 10 14:57:15 2020
 
 @author: zoro
 """
 
+#Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+#import data set
+
+dataset = pd.read_csv('Data.csv')
