f9f958023d9ebc445f4ed270fa49d13bbd611364
jamescardona11 <jjcardonao@gmail.com>
2020-08-10 15:08:21 -0500

    Init project from udemy course o develop basic concepts for Machine Learning

---
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 14:41:38 2020
+
+@author: zoro
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
