718c586cd965ab5853ef086afcacf1ac4a9f0fa8
jamescardona11 <jjcardonao@gmail.com>
2020-08-11 17:00:38 -0500

    Normalize values for py and R

--- Part 1 - Data Preprocessing/data_preprocessing.R
+++ Part 1 - Data Preprocessing/data_preprocessing.R
@@ -26,3 +26,27 @@ set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 testing_set = subset(dataset, split == FALSE)
+
+#normalized values
+training_set[,2:3] = scale(training_set[,2:3])
+testing_set[,2:3] = scale(testing_set[,2:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -39,5 +39,10 @@ y = labelEncoder_Y.fit_transform(y)
 
 #Split dataset in two subset 'Testing' and 'Training'
 from sklearn.cross_validation import train_test_split
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2,random_state = 0)
+
+#Scaling var
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
